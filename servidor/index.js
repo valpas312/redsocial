79b1037c8485a7410396e96d251e6ff764cf19a6
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-04-01 23:49:02 -0300

    asd

--- servidor/index.js
+++ servidor/index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use("/api", router);
 
 connect();
 
@@ -16,4 +17,6 @@ app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
 
-app.use("/api", router);
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
