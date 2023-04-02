5ba7d22ff8157cf48e152c58cf8742a2d71dd544
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-04-02 00:06:54 -0300

    listen

--- servidor/index.js
+++ servidor/index.js
@@ -13,6 +13,10 @@ app.use("/api", router);
 
 connect();
 
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
